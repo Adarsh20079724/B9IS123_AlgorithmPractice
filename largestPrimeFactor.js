061ed055b99166608602f67df982119c9ea1e3e1
Adarsh20079724 <20079724@mydbs.ie>
2025-09-30 22:18:15 +0100

    Created a function to finding prime factors.

---
+++ largestPrimeFactor.js
@@ -0,0 +1,9 @@
+//Find the Largest Prime Factors of a number.
+
+let randomNumber = prompt("Enter your desired number: ", "1234")
+
+const findPrimeFactors = (num) => {
+    
+}
+
+findPrimeFactors(randomNumber)
