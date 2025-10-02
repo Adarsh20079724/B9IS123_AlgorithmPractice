eea9898d7f16fce1a921e0fa14523096642ab79a
Adarsh20079724 <20079724@mydbs.ie>
2025-10-02 15:28:19 +0100

    Palindrom checking method created.

--- largestPalindromeProduct.js
+++ largestPalindromeProduct.js
@@ -1,1 +1,19 @@
-//
+const randomNumber = parseInt(prompt("Enter any Natural Number: ", "1234"));
+
+//const staticNumber = 12321
+
+const checkPalindrome = (num) => {
+    let isPalindrome = false
+    
+    if(num >= 0) {
+    let text = num.toString()
+    text != text.split("").reverse().join("") ? isPalindrome = false : isPalindrome = true
+    }
+    else{
+        console.log("Please enter positive integer")
+    }
+    
+    return isPalindrome
+}
+
+console.log("palindrome or not : ", checkPalindrome(randomNumber));
