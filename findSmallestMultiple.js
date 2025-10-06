715b27d2f6a35699b8bbebd13f4c9c8086e51d47
Adarsh20079724 <20079724@mydbs.ie>
2025-10-06 09:07:11 +0100

    Updated incorrect logic. Still needs to work on it

--- findSmallestMultiple.js
+++ findSmallestMultiple.js
@@ -2,19 +2,39 @@ console.log("---------Find the Smallest Multiple----------")
 
 const range = 10;
 
-const findSmallestMultiple = (limit) => {
-    let multiple = 1; 
-    let factorArr = []
-    for(let i = 2; i <= limit; i++)
-    {
-        multiple *= i
-        factorArr.push(i) 
-        console.log(factorArr)
+const checkExistingElement = (element, arr) => {
+    let checker = [...arr];
+    console.log("checker: ", checker)
+    let prod = 1;
+    let pushElement = 0;
+    for(let i = 0; i < checker.length; i++ ) {
+        prod = prod * checker[i]
     }
-    
-    
-    
-    return console.log(multiple)
+    console.log("prod: ", prod)
+    pushElement = prod % element;
+     
+    return console.log(pushElement)
 }
 
-findSmallestMultiple(range)
+checkExistingElement(5 , [ 2, 3, 2]);
+
+
+console.log(math.lcm([2, 3, 4, 5]))
+
+// const findSmallestMultiple = (limit) => {
+//     let multiple = 1; 
+//     let factorArr = []
+//     for(let i = 2; i <= limit; i++)
+//     {
+//         multiple *= i
+//         // if(factorArr.includes(i))
+//         factorArr.push(i) 
+//         console.log(factorArr)
+//     }
+    
+    
+    
+//     return console.log(multiple)
+// }
+
+// findSmallestMultiple(range)
